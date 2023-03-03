c56b27b945cdbd3f73ac9badfce39bdf6b74a111
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2023-03-03 15:13:35 +0530

    Update Pattern.java

--- Pattern.java
+++ Pattern.java
@@ -1,14 +1,16 @@
 import java.lang.*;
 import java.util.*;
-class Pattern{
-  public static void main{
-    Scanner s =new Scanner(System.in);
-    System.out.println("enter no of lines:");
-    int n=s.nextInt();
-    for(int i=1;i<=n;i++){
-      for(int j=1;j<=i;j++){
-        System.out.println("* ");
-      }
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner s =new Scanner(System.in);
+        System.out.println("enter no of lines:");
+        int n=s.nextInt();
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+
+        }
     }
-  } 
 }
