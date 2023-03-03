9fcc8d202e61717259755228a6465b00f47e2190
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2023-03-03 19:55:33 +0530

    Create conversion.java

---
+++ conversion.java
@@ -0,0 +1,6 @@
+/* BinToHex :- to get binary number "Integer.parseInt(s.nextLine(),2):"     to convert to hex use "Integer.toHexString()"
+   BintoDec :- to convert to dec "decimal=Integer.parseInt("1011",2); "  
+   DectoBin:-  to convert to bin "Integer.toBinaryString(decimal); " 
+   HextoDec to convert hex to dec " d=Integer.parseInt("15a",16);"          to get hex "Integer.parseInt(s.nextLine(),16);
+
+/* 
