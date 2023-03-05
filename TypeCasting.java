f920f1d5f19a0a02e2a61e94ed72823eab0451c5
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2023-03-05 19:56:30 +0530

    Create TypeCasting.java

---
+++ TypeCasting.java
@@ -0,0 +1,24 @@
+import java.lang.*;
+import java.util.*;
+/* 
+
+
+*/
+
+public class TypeCasting{
+    public static void main(String[] args){
+        Scanner s= new Scanner(System.in);
+        System.out.println("enter integer: ");
+        int n = s.nextInt();
+        double d= n;
+        System.out.println(" double value of given integer is " +d);
+        
+
+    }
+}
+
+/*
+
+
+
+*/
