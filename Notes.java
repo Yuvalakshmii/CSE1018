28a7dcb9fa1cdcaae5e743d70e9c3b07022d2d59
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2023-03-05 21:31:21 +0530

    Update and rename Useful.java to Notes.java

---
+++ Notes.java
@@ -0,0 +1,27 @@
+/*
+//Generate random double value from min=200 to max=400   ---> double a = Math.random()*(max-min+1)+min;   
+
+//8 Primitive Data Types
+1. boolean type
+2. byte type  --- can have values from -128 to 127
+3. short ---- can have values from -32768 to 32767
+4. int
+5. long
+6. double
+7. float
+8. char
+
+//Java String Operations, Methods
+1. length() ---- int length = greet.length();
+2. Join Two Java Strings ---String joinedString = first.concat(second);
+3. Compare two Strings ---  boolean result1 = first.equals(second);
+4. use the escape character  --- String example = "This is the \"String\" class.";
+5. valueOf() Return Value
+returns the string representation of the argument passed
+result = String.valueOf(languages);  
+System.out.println(result);
+6. toString() method converts the object into a string and returns it.  --- System.out.println(array[0].toString());
+7. parseInt() --- converts string to integer --- usage: Binary to decimal 
+8.
+
+*/
--- Useful.java
+++
@@ -1,2 +0,0 @@
-/*
-//Generate random double value from min=200 to max=400   ---> double a = Math.random()*(max-min+1)+min;   
