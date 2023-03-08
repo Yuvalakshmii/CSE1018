5b8c2a0f97ba84eb058dfc499ba129d848daa3d9
Yuva <114496360+Yuvalakshmii@users.noreply.github.com>
2023-03-08 14:34:21 +0530

    Update class_obj.java

--- class_obj.java
+++ class_obj.java
@@ -1,4 +1,4 @@
-//1. simple parameterised constructor/obj/class
+//1. simple parameterised constructor/obj/class -- OBJ USING "NEW" KEYWORD
 public class Main {
   public Main(int modelYear, String modelName) {
     System.out.println(modelYear + " " + modelName);
@@ -6,4 +6,15 @@ public class Main {
   public static void main(String[] args) {
     Main myCar = new Main(2022 , "KIA-Seltos");
   }
+}
+
+//2. OBJ USING ANONYMOUS WAY
+public class Stu{
+    public void insert(int r, String n){
+        System.out.println(r +" "+n);
+    }
+    public static void main(String[] args){
+       new Stu().insert(111, "karan");
+       new Stu().insert(222, "Aryan");
+    }
 }
